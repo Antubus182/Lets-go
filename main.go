98c0e04b419c86c7f8c6f109195922173f597697
Niels ten Thije <niels.ten.thije@zeton.nl>
2024-03-01 16:21:52 +0100

    2.3 Routing Requests

--- main.go
+++ main.go
@@ -9,14 +9,35 @@ import (
 var port string = ":4000"
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// because "/" is a subtree or catchall, we need to manually restrict access
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		log.Print("Accessed illegal, returned not found")
+		return
+	}
 	w.Write([]byte("Hello from Golang"))
+	log.Print("accessed home")
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Write a snippet"))
+	log.Print("accessed view")
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new Snippet"))
+	log.Print("accessed create")
 }
 
 func main() {
 	fmt.Println("Hello Snippet")
 
+	//Using a serveMux is good practise because we can define all routes here instead of having many http handlefuncs
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on " + port)
 	//ListenAndServe takes the port and the mux
