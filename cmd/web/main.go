a412ea40164f8608233f09273d68731c2433a9dd
Niels ten Thije <niels.ten.thije@zeton.nl>
2024-03-06 14:10:23 +0100

    failing database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -22,6 +25,7 @@ func main() {
 	// variable. You need to call this *before* you use the addr variable
 	// otherwise it will always contain the default value of ":4000". If any errors are
 	// encountered during parsing the application will be terminated.
+	dsn := flag.String("dsn", "admin:Onveilig41@tcp(test.antubus.nl)/letsgo?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logLevel := new(slog.LevelVar)
@@ -36,9 +40,38 @@ func main() {
 		logger: logger,
 	}
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	} else {
+		logger.Info("verbonden met database")
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
+
 	logger.Info("Starting server", "port", *addr)
 	//ListenAndServe takes the port and the mux
-	err := http.ListenAndServe(*addr, app.muxroutes())
+	err = http.ListenAndServe(*addr, app.muxroutes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("opened")
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	fmt.Println("pinged")
+
+	return db, nil
 }
